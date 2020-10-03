96f04b37e9ce7906a6991d48eb35adf7d6fcea80
zkan <zkan.cs@gmail.com>
2020-10-03 15:09:53 +0700

    Create tweet model :bear:

--- sinks/my_app/sentiments/models.py
+++ sinks/my_app/sentiments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from django_extensions.db.models import TimeStampedModel
+
+
+class Tweet(TimeStampedModel):
+    text = models.TextField()
+    search_term = models.CharField(max_length=300)
+
+    SENTIMENT_CHOICES = (
+        ('positive', 'Positive'),
+        ('neutral', 'Neutral'),
+        ('negative', 'Negative'),
+    )
+    sentiment = models.CharField(
+        max_length=10,
+        choices=SENTIMENT_CHOICES,
+        default='neutral',
+    )
---
+++ sinks/my_app/sentiments/tests/test_models.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+from ..models import Tweet
+
+
+class TestTweet(TestCase):
+    def test_it_should_have_defined_fields(self):
+        Tweet.objects.create(
+            text='This is my first tweet!',
+            search_term='first tweet',
+            sentiment='positive'
+        )
+
+        tweet = Tweet.objects.last()
+
+        self.assertEqual(tweet.text, 'This is my first tweet!')
+        self.assertEqual(tweet.search_term, 'first tweet')
+        self.assertEqual(tweet.sentiment, 'positive')
+        self.assertIsNotNone(tweet.created)
+        self.assertIsNotNone(tweet.modified)
+
+    def test_sentiment_field_should_set_choices(self):
+        expected_choices = (
+            ('positive', 'Positive'),
+            ('neutral', 'Neutral'),
+            ('negative', 'Negative'),
+        )
+        self.assertEqual(Tweet.sentiment.field.choices, expected_choices)
