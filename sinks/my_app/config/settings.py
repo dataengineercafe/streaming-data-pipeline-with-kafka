c517bd0192b564a48a536bc5e19456f635df24e9
zkan <zkan.cs@gmail.com>
2020-10-04 11:49:17 +0700

    :card_file_box: Connect to Postgres :bear:

--- sinks/my_app/config/settings.py
+++ sinks/my_app/config/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'my_app',
+        'USER': 'zkan',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
