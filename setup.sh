3a23a995d0d3968ef126fd0b938d79b8279d8128
zkan <zkan.cs@gmail.com>
2021-08-31 21:36:58 +0700

    Update Kafka version :bear:

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -O https://downloads.apache.org/kafka/2.6.0/kafka_2.13-2.6.0.tgz
-tar -xvf kafka_2.13-2.6.0.tgz
-cd kafka_2.13-2.6.0
+curl -O https://dlcdn.apache.org/kafka/2.8.0/kafka_2.13-2.8.0.tgz
+tar -xvf kafka_2.13-2.8.0.tgz
+cd kafka_2.13-2.8.0
